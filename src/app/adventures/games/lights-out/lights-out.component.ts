21b050384a7300bd99f6e3350a472d50a525b258
dudushy <eduardotalarico1337@gmail.com>
2023-11-15 02:17:41 -0300

    Update lights-out game
    - count clicks
    - count seconds
    - match patterns

--- src/app/adventures/games/lights-out/lights-out.component.ts
+++ src/app/adventures/games/lights-out/lights-out.component.ts
@@ -10,9 +10,106 @@ import { AppComponent } from '../../../app.component';
 export class LightsOutComponent implements OnInit {
   title = 'LightsOutComponent';
 
+  gameEnd = false;
   rows = 5;
   columns = 5;
   randomLights = this.rows + this.columns;
+  counterClicks = 0;
+  counterTime = -1;
+  randomPattern = 'chess';
+  patterns = {
+    // 'pattern-name': [
+    //   ['O', 'O', 'O', 'O', 'O'],
+    //   ['O', 'O', 'O', 'O', 'O'],
+    //   ['O', 'O', 'X', 'O', 'O'],
+    //   ['O', 'O', 'O', 'O', 'O'],
+    //   ['O', 'O', 'O', 'O', 'O'],
+    // ],
+    'chess': [
+      ['X', 'O', 'X', 'O', 'X'],
+      ['O', 'X', 'O', 'X', 'O'],
+      ['X', 'O', 'X', 'O', 'X'],
+      ['O', 'X', 'O', 'X', 'O'],
+      ['X', 'O', 'X', 'O', 'X'],
+    ],
+    'horizontal-line': [
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+    ],
+    'edges': [
+      ['X', 'X', 'O', 'O', 'X'],
+      ['O', 'O', 'O', 'O', 'X'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['X', 'O', 'O', 'O', 'O'],
+      ['X', 'O', 'O', 'X', 'X'],
+    ],
+    'horizontal-dots': [
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+    ],
+    'corners': [
+      ['X', 'O', 'O', 'O', 'X'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['X', 'O', 'O', 'O', 'X'],
+    ],
+    'center-dot': [
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+    ],
+    'almost-corners-dots': [
+      ['O', 'X', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'X'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['X', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'X', 'O'],
+    ],
+    'square': [
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'X', 'X', 'X', 'O'],
+      ['O', 'X', 'X', 'X', 'O'],
+      ['O', 'X', 'X', 'X', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+    ],
+    'center-dot-inverted': [
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'O', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+    ],
+    'rubiks-cube': [
+      ['X', 'O', 'X', 'O', 'X'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['X', 'O', 'X', 'O', 'X'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['X', 'O', 'X', 'O', 'X'],
+    ],
+    'diagonal': [
+      ['X', 'O', 'O', 'O', 'O'],
+      ['O', 'X', 'O', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'O', 'X', 'O'],
+      ['O', 'O', 'O', 'O', 'X'],
+    ],
+    'filled': [
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X', 'X'],
+    ],
+  };
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -23,17 +120,17 @@ export class LightsOutComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(`[${this.title}#ngOnInit]`);
+    this.chooseRandomPattern();
   }
 
   ngAfterViewInit() {
     console.log(`[${this.title}#ngAfterViewInit]`);
+    this.setupRandomPattern();
   }
 
-  ngAfterViewChecked() {
-    console.log(`[${this.title}#ngAfterViewChecked]`);
-
-    this.randomToggle();
-  }
+  // ngAfterViewChecked() {
+  //   console.log(`[${this.title}#ngAfterViewChecked]`);
+  // }
 
   updateView() {
     console.log(`[${this.title}#updateView]`);
@@ -51,6 +148,10 @@ export class LightsOutComponent implements OnInit {
   defaultOrder() { return 0; }
 
   toggleLights(event) {
+    if (this.gameEnd) return;
+
+    if (this.counterTime == -1) this.startTimer();
+
     console.log(`[${this.title}#toggleLights] event`, event);
 
     const id = event.target.id;
@@ -59,16 +160,16 @@ export class LightsOutComponent implements OnInit {
     const cellIndex = parseInt(id.split('-')[1]);
     console.log(`[${this.title}#toggleLights] cellIndex`, cellIndex);
 
-    const rightLight = document.getElementById(`cell-${cellIndex + 1}`);
+    const rightLight = document.getElementById(`gridCell-${cellIndex + 1}`);
     console.log(`[${this.title}#toggleLights] rightLight`, rightLight);
 
-    const leftLight = document.getElementById(`cell-${cellIndex - 1}`);
+    const leftLight = document.getElementById(`gridCell-${cellIndex - 1}`);
     console.log(`[${this.title}#toggleLights] leftLight`, leftLight);
 
-    const topLight = document.getElementById(`cell-${cellIndex - this.columns}`);
+    const topLight = document.getElementById(`gridCell-${cellIndex - this.columns}`);
     console.log(`[${this.title}#toggleLights] topLight`, topLight);
 
-    const bottomLight = document.getElementById(`cell-${cellIndex + this.columns}`);
+    const bottomLight = document.getElementById(`gridCell-${cellIndex + this.columns}`);
     console.log(`[${this.title}#toggleLights] bottomLight`, bottomLight);
 
     const detectRightBorder = cellIndex % this.columns === 0;
@@ -82,6 +183,9 @@ export class LightsOutComponent implements OnInit {
     if (!detectLeftBorder) leftLight?.classList.toggle('on');
     topLight?.classList.toggle('on');
     bottomLight?.classList.toggle('on');
+
+    this.checkPatternMatch();
+    this.counterClicks++;
   }
 
   randomToggle() {
@@ -100,5 +204,82 @@ export class LightsOutComponent implements OnInit {
     }
 
     this.updateView();
+  }
+
+  chooseRandomPattern() {
+    console.log(`[${this.title}#chooseRandomPattern] patterns`, this.patterns);
+
+    this.randomPattern = Object.keys(this.patterns)[Math.floor(Math.random() * Object.keys(this.patterns).length)];
+    console.log(`[${this.title}#chooseRandomPattern] randomPattern`, this.randomPattern);
+  }
+
+  setupRandomPattern() {
+    this.patterns[this.randomPattern].forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        const id = `patternCell-${(rowIndex * this.columns) + cellIndex + 1}`;
+        console.log(`[${this.title}#setupRandomPattern] id`, id);
+
+        const cellElement = document.getElementById(id);
+        console.log(`[${this.title}#setupRandomPattern] cellElement`, cellElement);
+
+        if (cell === 'X') {
+          cellElement?.classList.add('on');
+        } else {
+          cellElement?.classList.remove('on');
+        }
+      });
+    });
+  }
+
+  checkPatternMatch() {
+    console.log(`[${this.title}#checkPatternMatch] patterns`, this.patterns);
+
+    let match = true;
+
+    this.patterns[this.randomPattern].forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        const id = `gridCell-${(rowIndex * this.columns) + cellIndex + 1}`;
+        console.log(`[${this.title}#checkPatternMatch] id`, id);
+
+        const cellElement = document.getElementById(id);
+        console.log(`[${this.title}#checkPatternMatch] cellElement`, cellElement);
+
+        if (cell === 'X') {
+          if (!cellElement?.classList.contains('on')) {
+            match = false;
+          }
+        } else {
+          if (cellElement?.classList.contains('on')) {
+            match = false;
+          }
+        }
+      });
+    });
+
+    if (match) {
+      console.log(`[${this.title}#checkPatternMatch] match`, match);
+
+      this.gameEnd = true;
+
+      setTimeout(() => {
+        alert(`You won! It took you ${this.counterClicks} clicks and ${this.counterTime} seconds!`);
+      }, 100);
+    }
+  }
+
+  startTimer() {
+    console.log(`[${this.title}#startTimer]`);
+
+    this.counterTime = 0;
+
+    setInterval(() => {
+      if (this.gameEnd) return;
+
+      console.log(`[${this.title}#startTimer] (BEFORE) counterTime`, this.counterTime);
+
+      this.counterTime++;
+
+      console.log(`[${this.title}#startTimer] (AFTER) counterTime`, this.counterTime);
+    }, 1000);
   }
 }

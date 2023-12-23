1f2add28a7ea33e6d9804f06dbca6b299b98e688
dudushy <eduardotalarico1337@gmail.com>
2023-12-23 20:59:01 -0300

    Create button animations and pull gesture

--- src/app/adventures/clones/iphone-lockscreen/iphone-lockscreen.component.ts
+++ src/app/adventures/clones/iphone-lockscreen/iphone-lockscreen.component.ts
@@ -13,6 +13,16 @@ export class IphoneLockscreenComponent implements OnInit {
   date = 'Thursday, December 21';
   time = '10:27';
 
+  pullGestureConfig = {
+    startY: 0,
+    currentY: 0,
+    isDragging: false,
+    translateY: 0,
+    // threshold: 100,
+    threshold: -100,
+    multiplier: 0.5
+  };
+
   constructor(
     private cdr: ChangeDetectorRef,
     public app: AppComponent
@@ -22,6 +32,14 @@ export class IphoneLockscreenComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(`[${this.title}#ngOnInit]`);
+
+    this.updateDateAndTime();
+
+    setInterval(() => {
+      this.updateDateAndTime();
+    }, 1000 * 30);
+
+    this.setupPullGesture();
   }
 
   updateView() {
@@ -35,5 +53,160 @@ export class IphoneLockscreenComponent implements OnInit {
     await this.app.redirectTo(url, this.title);
 
     this.updateView();
+  }
+
+  updateDateAndTime() {
+    console.log(`[${this.title}#updateDateAndTime]`);
+
+    const date = new Date();
+    console.log(`[${this.title}#updateDateAndTime] date`, date);
+
+    const formatedDate = date.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+    console.log(`[${this.title}#updateDateAndTime] formatedDate`, formatedDate);
+
+    this.date = formatedDate;
+    console.log(`[${this.title}#updateDateAndTime] this.date`, this.date);
+
+    const formatedTime = date.toLocaleTimeString('pt-br', { hour: 'numeric', minute: 'numeric' });
+    console.log(`[${this.title}#updateDateAndTime] formatedTime`, formatedTime);
+
+    this.time = formatedTime;
+    console.log(`[${this.title}#updateDateAndTime] this.time`, this.time);
+  }
+
+  setupPullGesture() {
+    console.log(`[${this.title}#setupPullGesture]`);
+
+    const lockscreen = document.getElementById('lockscreen');
+    console.log(`[${this.title}#setupPullGesture] lockscreen`, lockscreen);
+
+    const triggerDiv = document.getElementById('pullGestureTrigger');
+    console.log(`[${this.title}#setupPullGesture] triggerDiv`, triggerDiv);
+
+    //? Desktop Version
+    triggerDiv.addEventListener('mousedown', (event) => {
+      console.log(`[${this.title}#setupPullGesture#mousedown] event`, event);
+
+      // event.preventDefault();
+
+      const startY = event.clientY;
+      console.log(`[${this.title}#setupPullGesture#mousedown] startY`, startY);
+
+      this.pullGestureConfig.startY = startY;
+      this.pullGestureConfig.isDragging = true;
+
+      console.log(`[${this.title}#setupPullGesture#mousedown] this.pullGestureConfig`, this.pullGestureConfig);
+    });
+
+    lockscreen.addEventListener('mousemove', (event) => {
+      // console.log(`[${this.title}#setupPullGesture#mousemove] event`, event);
+
+      // event.preventDefault();
+
+      if (!this.pullGestureConfig.isDragging) return;
+
+      console.log(`[${this.title}#setupPullGesture#mousemove] event`, event);
+
+      // event.preventDefault();
+
+      const currentY = event.clientY;
+      console.log(`[${this.title}#setupPullGesture#mousemove] currentY`, currentY);
+
+      this.pullGestureConfig.currentY = currentY;
+
+      const diffY = currentY - this.pullGestureConfig.startY;
+      console.log(`[${this.title}#setupPullGesture#mousemove] diffY`, diffY);
+
+      if (diffY > 0) return;
+
+      // this.pullGestureConfig.translateY = diffY;
+      this.pullGestureConfig.translateY = diffY * (1 + this.pullGestureConfig.multiplier * (1 - this.pullGestureConfig.translateY / window.innerHeight));
+
+      console.log(`[${this.title}#setupPullGesture#mousemove] this.pullGestureConfig`, this.pullGestureConfig);
+    });
+
+    lockscreen.addEventListener('mouseup', (event) => {
+      console.log(`[${this.title}#setupPullGesture#mouseup] event`, event);
+
+      // event.preventDefault();
+
+      if (!this.pullGestureConfig.isDragging) return;
+
+      this.pullGestureConfig.isDragging = false;
+
+      // if (this.pullGestureConfig.translateY >= this.pullGestureConfig.threshold) {
+      if (this.pullGestureConfig.translateY <= this.pullGestureConfig.threshold) {
+        alert('You unlocked your iPhone!');
+
+        this.pullGestureConfig.translateY = 0;
+      } else {
+        this.pullGestureConfig.translateY = 0;
+      }
+
+      console.log(`[${this.title}#setupPullGesture#mouseup] this.pullGestureConfig`, this.pullGestureConfig);
+    });
+
+    //? Mobile Version
+    triggerDiv.addEventListener('touchstart', (event) => {
+      console.log(`[${this.title}#setupPullGesture#touchstart] event`, event);
+
+      event.preventDefault();
+
+      const startY = event.touches[0].clientY;
+      console.log(`[${this.title}#setupPullGesture#touchstart] startY`, startY);
+
+      this.pullGestureConfig.startY = startY;
+      this.pullGestureConfig.isDragging = true;
+
+      console.log(`[${this.title}#setupPullGesture#touchstart] this.pullGestureConfig`, this.pullGestureConfig);
+    });
+
+    lockscreen.addEventListener('touchmove', (event) => {
+      // console.log(`[${this.title}#setupPullGesture#touchmove] event`, event);
+
+      // event.preventDefault();
+
+      if (!this.pullGestureConfig.isDragging) return;
+
+      console.log(`[${this.title}#setupPullGesture#touchmove] event`, event);
+
+      event.preventDefault();
+
+      const currentY = event.touches[0].clientY;
+      console.log(`[${this.title}#setupPullGesture#touchmove] currentY`, currentY);
+
+      this.pullGestureConfig.currentY = currentY;
+
+      const diffY = currentY - this.pullGestureConfig.startY;
+      console.log(`[${this.title}#setupPullGesture#touchmove] diffY`, diffY);
+
+      if (diffY > 0) return;
+
+      // this.pullGestureConfig.translateY = diffY;
+      this.pullGestureConfig.translateY = diffY * (1 + this.pullGestureConfig.multiplier * (1 - this.pullGestureConfig.translateY / window.innerHeight));
+
+      console.log(`[${this.title}#setupPullGesture#touchmove] this.pullGestureConfig`, this.pullGestureConfig);
+    });
+
+    lockscreen.addEventListener('touchend', (event) => {
+      console.log(`[${this.title}#setupPullGesture#touchend] event`, event);
+
+      event.preventDefault();
+
+      if (!this.pullGestureConfig.isDragging) return;
+
+      this.pullGestureConfig.isDragging = false;
+
+      // if (this.pullGestureConfig.translateY >= this.pullGestureConfig.threshold) {
+      if (this.pullGestureConfig.translateY <= this.pullGestureConfig.threshold) {
+        alert('You unlocked your iPhone!');
+
+        this.pullGestureConfig.translateY = 0;
+      } else {
+        this.pullGestureConfig.translateY = 0;
+      }
+
+      console.log(`[${this.title}#setupPullGesture#touchend] this.pullGestureConfig`, this.pullGestureConfig);
+    });
   }
 }
